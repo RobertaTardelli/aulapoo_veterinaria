13f67a502ee4053a8499b432b829f44a959f0d0c
Rodrigo <digocoutinho@outlook.com>
2023-08-23 12:01:32 -0300

    gets e sets de clinica

--- veterinaria/src/main/java/com/poo/veterinaria/classes/Tutor.java
+++ veterinaria/src/main/java/com/poo/veterinaria/classes/Tutor.java
@@ -2,48 +2,53 @@ package com.poo.veterinaria.classes;
 
 public class Tutor {
 
-public String nome ;
-public int cpf;
-public String endereco;
-public int telefone;
-public String email;
+    public String nome;
+    public int cpf;
+    public String endereco;
+    public int telefone;
+    public String email;
 
-public String getNome() {
-return nome;
-}
-public void setNome(String nome) {
-this.nome = nome;
-}
+    public String getNome() {
+        return nome;
+    }
 
-public int getCpf (){
-return cpf;
-}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-public void setCpf(int cpf){
-this.cpf = cpf;
+    public int getCpf() {
+        return cpf;
+    }
 
-}
-public String getEndereco (){
-return  endereco;
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
 
-}
-public void  setEndereco(String endereco){
-this.endereco = endereco;
+    }
 
-}
-public int getTelefone (){
-return telefone;
-}
+    public String getEndereco() {
+        return endereco;
 
-public void setTelefone(int telefone){
-this.telefone = telefone
-}
+    }
 
-public String getEmail (){
-return email;
-}
-public void setEmail(String email){
-this.email = email;
-}
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
