1ac64ca4fc37e845774cf83d16e18a0f852c00f5
RobertaTardelli <robertatardelli@gmail.com>
2023-08-23 10:08:25 -0300

    criacao de tutor

--- veterinaria/src/main/java/com/poo/veterinaria/classes/ClinicaVeterinaria.java
+++ veterinaria/src/main/java/com/poo/veterinaria/classes/ClinicaVeterinaria.java
@@ -1,11 +1,12 @@
 package com.poo.veterinaria.classes;
 
 public class ClinicaVeterinaria {
-<<<<<<< HEAD
-    public nome
     public String CNPJ;
-=======
-    public String nome;
->>>>>>> 9d9722a6c99a80ba7833a2e9d9ff8d445ab48e02
+    public String razaoSocial;
+    public String endereco;
+    public String tefone ;
+
+
+    
 }
 
---
+++ veterinaria/src/main/java/com/poo/veterinaria/classes/Tutor.java
@@ -0,0 +1,13 @@
+package com.poo.veterinaria.classes;
+
+public class Tutor {
+
+public String nome ;
+public int cpf;
+public String endereco;
+public int telefone;
+public String email;
+
+
+
+}
